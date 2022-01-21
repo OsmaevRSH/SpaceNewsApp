4b4af9b4e3bd400fcea93fbe979f1e23311b1365
Ruslan Osmaev <osmaev.ruslan.bmstu@gmail.com>
2022-01-21 15:08:58 +0300

    Initial Commit

---
+++ SpaceNewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpaceNewsApp
+//
+//  Created by Руслан Осмаев on 21.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
