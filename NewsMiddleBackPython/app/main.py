14d2f28f74bd2a0504b48e9abc3b949369852a08
Ruslan Osmaev <osmaevrsh@gmail.com>
2022-02-10 01:53:14 +0300

    added python server

---
+++ NewsMiddleBackPython/app/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from newspaper import Article
+
+app = FastAPI()
+print("Start")
+
+
+@app.get("/description")
+async def description(url: str):
+    return get_description(url)
+
+
+def get_description(url):
+    article = Article(url)
+    article.download()
+    article.parse()
+    return article.text
